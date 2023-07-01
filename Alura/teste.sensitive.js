4af8c12703908cd75b1e1d57171f9ecea278a20a
Lucas Nascimento <lucasvelcks@gmail.com>
2023-06-30 23:02:42 -0300

    case.sensitive

--- Alura/teste.sensitive.js
+++ Alura/teste.sensitive.js
@@ -2,5 +2,6 @@ const minhaVar = 1;
 const MinhaVar = "texto";
 const minhavar = "3";
 const MINHAVAR = 2;
+const MINHAVAr = 4; 
 
-console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR, MINHAVAr)
