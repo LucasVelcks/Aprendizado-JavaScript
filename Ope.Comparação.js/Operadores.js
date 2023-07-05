74c3ade6c2ab63854df0c2f6319212b92b6c91dd
Lucas Nascimento <lucasvelcks@gmail.com>
2023-07-04 21:32:22 -0300

    Operadores de Comparação

---
+++ Ope.Comparação.js/Operadores.js
@@ -0,0 +1,13 @@
+// > (Maior que)
+// >= (Maior que ou igual a)
+// < (Menor que)
+// <= (Menor que ou igual a)
+// == (igualdade) - Checa valor *NÃO RECOMENDAVEL*
+// === (igualdade estrita) - Checa valor e tipo
+// != (diferente) - Checa valor *NÃO RECOMEDAVEL*
+// !== (diferente estrito) Checa valor e tipo
+
+const num1 = 10; // Number
+const num2 = '10'; // String
+const comparação = num1 == num2; // (==) NÃO DIFERENCIA O TIPO, ASSIM COMO (!=).
+console.log(comparação);
